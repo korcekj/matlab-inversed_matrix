2d247f82893f120f373944b2c0ec8ee0f5994a8c
Jan Korcek <korcekdevelopment@yahoo.com>
2019-03-05 09:05:34 +0100

    Comments have been updated.

--- fitness.m
+++ fitness.m
@@ -1,15 +1,15 @@
-% Fitness function of polynomial regression
+% Fitness function of matrix inversion
 % Search space: -5 < x(i) < 5
 
 function [Fit] = fitness(Pop, A)
 
-    [lpop,lstring]=size(Pop); % Zistenie velkosti matrice Pop -> pocet riadkov, stlpcov
-    invA=inv(A);
-    invA=invA(:)';
+    [lpop,lstring]=size(Pop); % zistenie velkosti matrice Pop -> pocet riadkov, stlpcov
+    invA=inv(A); % vypocet inverznej matice z matice A ako parameter funckie
+    invA=invA(:)'; % transponovanie stlpca na riadok kvoli jednoduchsiemu vypoctu
     
     for i=1:lpop
-        diff=abs(Pop(i,:)-invA);
-        Fit(i)=sum(diff(:));
+        diff=abs(Pop(i,:)-invA); % vypocitanie rozdielu medzi vektorom hodnot inveznych matic
+        Fit(i)=sum(diff(:)); % spocitanie rozdielov hodnot a priradenie hodnoty na riadok i
     end
     
 end
--- inversion.m
+++ inversion.m
@@ -1,25 +1,25 @@
-A=[-1 2 0; 4 -3 1; 1 4 2]; 
-invA=inv(A);
+A=[-1 2 0; 4 -3 1; 1 4 2]; % matica 3x3
+invA=inv(A); % invezna matica pre porovnanie inverznich matic
 
 numcykle=1000;	% pocet generacii
 lpop=30; % velkost populacie
 Space=[ones(1,9)*(-5);ones(1,9)*5]; % rozsah hodnot urceny od -5 do 5
 Pop=genrpop(lpop,Space); % generovanie populacie z daneho rozsahu
 
-Fit=fitness(Pop, A);
+Fit=fitness(Pop, A); % vyhodnotenie funkcie fitness
 [minFit, indx]=min(Fit); % najdenie najlepsieho (minimalneho) retazca
 minRet=Pop(indx,:); % ulozenie najlepsieho retazca do pomocnej premennej
-grafFit=zeros(1,numcykle);
+grafFit=zeros(1,numcykle); % vytvorenie grafu pre zobrazenie genetickeho algoritmu
 
 for i=1:numcykle
     NewPop1=selbest(Pop, Fit, [3 1]); % 3 najlepsich a 1 nahodny vyber prvkov
     NewPop2=selrand(Pop, Fit, 26); % nahodne vybrane ostatne chybajuce prvky do poppulacie
-    CrossedPop=crossov(NewPop2, 2, 0); % krizenie
-    MutedPopX=mutx(CrossedPop, 0.1, Space); % mutacia
-    MutedPopA=muta(MutedPopX, 0.02, [5 5 5 5 5 5 5 5 5]*0.02, Space); % mutacia
+    CrossedPop=crossov(NewPop2, 2, 0); % nahodne krizenie 2 parentov
+    MutedPopX=mutx(CrossedPop, 0.1, Space); % globalna mutacia
+    MutedPopA=muta(MutedPopX, 0.02, [5 5 5 5 5 5 5 5 5]*0.02, Space); % aditivna mutacia
     Pop=[NewPop1; MutedPopA]; % Spojenie najlepsich a zmutovanych jedincov
 
-    Fit=fitness(Pop, A);
+    Fit=fitness(Pop, A); % znovu vyhodnotenie fitness funkcie 
     [minFitnew,indx]=min(Fit); % najdenie najlepsieho (minimalneho) retazca
     if minFitnew<minFit % porovnanie min. a v pripade potreby zmena min. hodnoty
         minFit=minFitnew;
